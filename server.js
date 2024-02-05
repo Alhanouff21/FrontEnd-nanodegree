40b66394ba5acdb4d1a9fdd67b3b7482287ba702
Hanouf Alrekayan <hanofabduallh01@gmail.com>
2024-02-05 19:01:30 +0300

    Project finish

--- server.js
+++ server.js
@@ -1,19 +1,48 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+let projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
-/* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
+/* Middleware */
+// Here we are configuring express to use body-parser as middleware.
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Cors for cross origin allowance
+// Cors for cross-origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
+// GET route that returns the projectData object
+app.get('/all' ,(req,res)=>{
+
+    res.send(projectData)
+})
+
 
 // Setup Server
+const port = 8000;
+
+app.listen(port, () => {
+    console.log(`Server running on: http://localhost:${port}`);
+  });
+
+// POST route
+app.post('/weatherData' , (req , res)=>{
+
+    projectData.date = req.body.date;
+    projectData.temp = req.body.temp;
+    projectData.content = req.body.content;
+    projectData.clouds = req.body.clouds;
+    res.send(projectData);
+
+	console.log(projectData);
+
+})
--- website/app.js
+++ website/app.js
@@ -1,5 +1,88 @@
-/* Global Variables */
 
-// Create a new date instance dynamically with JS
+// Web API URL & KEY
+let urlbase = "https://api.openweathermap.org/data/2.5/weather?zip=";
+const APIkey = "&appid=cbd98d69c130d71eae74997d2f58d35b&units=metric";
+
+//date 
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + 1 + '.' + d.getDate() + '.' + d.getFullYear();
+
+// weather data with api 
+const performAction = () => {
+    const zip = document.getElementById('zip').value;
+    const content = document.getElementById('feelings').value;
+    
+    if (zip == '' || content == ''){
+      alert("There is a missing info in ZIP or FEELINGS please fill and retry");
+    }else{
+    getData(urlbase , zip , APIkey)
+        .then(function(data){
+
+            console.log(data)
+            const cloudsDescription = data.weather[0].description;
+            postData('/weatherData' , {temp : data.main.temp , date:newDate , content : content, clouds:cloudsDescription});
+        
+        })
+        .then(()=>{
+
+          updateUI();
+            
+        })
+        .catch(error => {
+            console.log("Error fetching data:", error);
+          });
+}}
+
+//event listener calling
+document.getElementById('generate').addEventListener('click', performAction);
+
+// get web api
+const getData = async (urlbase, zip, APIkey) => {
+    const url = urlbase + zip + APIkey;
+    const res = await fetch(url);
+    try {   
+      const data = await res.json();
+      return data;
+    } catch (error) {  
+      console.log("error", error);
+    }   
+  };
+
+
+// POST data
+const postData = async (url = "", data = {}) => {
+    const response = await fetch(url, {
+      method: "POST",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+     try {
+      const newData = await response.json();
+      return newData;
+    } catch (error) {
+      console.log("error", error);
+    }
+};
+
+
+
+// Update UI
+const updateUI = async () => {
+    const res = await fetch("/all");
+    try {
+        const alldata = await res.json();
+
+        document.getElementById("date").innerHTML = `Today is : ${alldata.date}`;
+        document.getElementById("temp").innerHTML = `Temprature now is: ${alldata.temp} °c`;
+        document.getElementById("content").innerHTML = alldata.content;
+        document.getElementById("clouds").innerHTML = alldata.clouds;
+
+
+    } catch (error) {
+        console.log("Error:", error);
+    }
+};
